f8030186578fe5e9082803606297b20a35d67e9e
Gabriel Muscalu <gabriel.muscalu@qualitance.com>
2016-11-23 15:21:36 +0200

    documentation - account module

--- client/app/account/signup/signup.directive.js
+++ client/app/account/signup/signup.directive.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('ldrWebApp')
+    /**
+     * @ngdoc directive
+     * @name checkRequired
+     * @description returns true if model value is true
+     * @example
+     * <pre><input type="checkbox" name="terms" ng-model="user.terms" id="checkbox1" check-required></pre>
+     */
     .directive('checkRequired', [function () {
         return {
             require: 'ngModel',
