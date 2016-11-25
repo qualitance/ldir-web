14820a6a67e52c15dc94a0df844cc3e306a2ec92
Gabriel Muscalu <gabriel.muscalu@qualitance.com>
2016-11-25 14:32:55 +0200

    documentation - privacy and terms modules

--- client/app/terms/terms.controller.js
+++ client/app/terms/terms.controller.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('ldrWebApp')
+    /**
+     * @ngdoc controller
+     * @name TermsCtrl
+     * @description terms controller
+     * @requires $http
+     * @requires $scope
+     * @requires $sce
+     * @requires $translate
+     * @property {terms} html containing terms and conditions
+     */
     .controller('TermsCtrl', ['$http', '$scope', '$sce', '$translate', function ($http, $scope, $sce, $translate) {
 
         $http.get('/assets/pages/terms_' + $translate.use() + '.html').success(function (data) {
